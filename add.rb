aaf5c20987a826d543797e831bb69e0f2faff7fe
root <root@201011JUNC0408.(none)>
2015-03-05 15:09:44 +0530

    Please enter the commit message for your changes. Lines starting

--- add.rb
+++ add.rb
@@ -1,7 +1,5 @@
 print 3+4
-gets
-
 first_name = "vignesh"
 last_name = "thiagarajan"
 fav_color = "blue"
-puts "My name is " + first_name + last_name + " My fav color is " + fav_color
+puts "My name is " + first_name + last_name + " My fav color is" + fav_color 
