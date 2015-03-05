e41b06ed29f82586eb977aa2c68dc0e624727c5a
root <root@201011JUNC0408.(none)>
2015-03-05 15:14:36 +0530

    changed and removed unwanted file bugs

--- add.rb
+++ add.rb
@@ -1,5 +1,5 @@
-print 3+4
+print  3+4 
 first_name = "vignesh"
 last_name = "thiagarajan"
 fav_color = "blue"
-puts "My name is " + first_name + last_name + " My fav color is" + fav_color 
+puts "My name is " + first_name + last_name + " My favourite color is" + fav_color 
